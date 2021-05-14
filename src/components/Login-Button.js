965a02afb602bbd68f6b9d864239af56f25ce6cb
Jacob Layton <jlayjlay3@gmail.com>
2021-05-14 14:44:04 -0700

    Update Login button functionality

--- src/components/Login-Button.js
+++ src/components/Login-Button.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import '../styles/components.css';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button id='login-button' onClick={() => loginWithRedirect({
+    redirectUri: 'http://localhost:3000/admin'
+  })}>Log In</button>;
 };
 
 export default LoginButton;
