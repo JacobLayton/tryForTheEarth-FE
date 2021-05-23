3563e4a856319aa4502d33845e9b9a52cf7c86f6
Jacob Layton <jlayjlay3@gmail.com>
2021-05-23 14:45:48 -0700

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import HamburgerButton from './HamburgerButton';
 import TFTELogo from '../img/TFTElogo.png';
+import tfteLogo from '../img/tfte-logo-sm-trimmy.png';
 import '../styles/nav-bar.css';
 
 
@@ -10,9 +11,11 @@ const Navbar = (props) => {
   return (
     <div className='navbarContainer'>
         <HamburgerButton handleClick={props.toggleMenu}/>
-        <Link to='/'>
-          <img src={TFTELogo} alt='Try for the earth logo' />
-        </Link>
+        <div className='navbar-logo'>
+          <Link to='/'>
+            <img src={tfteLogo} alt='Try for the earth logo' className='tfte-logo'/>
+          </Link>
+        </div>
     </div>
   );
 };
