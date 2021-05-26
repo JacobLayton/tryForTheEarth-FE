9324f4c995a845ec25103890862ab9c9465de659
Jacob Layton <jlayjlay3@gmail.com>
2021-05-26 14:47:15 -0700

    Update Admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,59 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import axios from "axios";
 import LogoutButton from '../components/Logout-Button';
 import PostCard from "../components/PostCard";
 import { useAuth0 } from "@auth0/auth0-react";
+import '../styles/home-page.css';
 
 
 function Admin(props) {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const history = useHistory();
+  console.log('User: ', user);
+  if (user) {
+    console.log('USer: ', user);
+    console.log('user email: ', user.email);
+    console.log('user email verified: ', user.email_verified);
+  } else {
+    history.push('/');
+  }
   const [posts, setPosts] = useState([]);
+  const location = useLocation();
 
-  // if (isLoading) {
-  //   return <div> LOADING.... </div>;
-  // }
+    useEffect( async () => {
+      let mounting = true;
+      axios.get('http://localhost:9001/posts')
+        .then(res =>  {
+          console.log('API CALLED', res);
+                  if(mounting) {
+                      setPosts(res.data);
+                  }
+        })
+        .catch(err => {
+          console.log('Error in get request', err);
+        })
+          return () => mounting = false;
+      }, []);
 
-  useEffect(() => {
-    let mounting = true;
-    axios.get('http://localhost:9001/posts')
-      .then(res =>  {
-        console.log('API CALLED', res);
-                if(mounting) {
-                    setPosts(res.data);
-                }
-      })
-      .catch(err => {
-        console.log('Error in get request', err);
-      })
-        return () => mounting = false;
-    }, []);
-
-    if (isLoading) {
-      return <div> LOADING.... </div>;
-    }
-
+      if (isLoading) {
+        return <div> LOADING.... </div>;
+      }
+  
   return (
     isAuthenticated ? (
-      <div>
-        <h1>Admin Page</h1>
-        <img src={user.picture} alt={user.name} />
-        <h2>Hello {user.name}</h2>
-        <p>{user.email}</p>
-        <LogoutButton />
-        <Link to='/createpost'>
-          <button>Create New Post</button>
-        </Link>
+      <div className='home-container'>
+        <div className='admin-buttons'>
+          <h1 className='admin-title'>Admin Page</h1>
+          <h2>Hello {user.name}</h2>
+          <LogoutButton />
+          <Link to='/createpost'>
+            <button className='admin-button'>Create New Post</button>
+          </Link>
+          <div className='line-break' />
+        </div>
         {posts.map(post => {
             return ( 
-                <Link to={`/adminpost/${post.id}`} key={post.id}>
-                    <PostCard post={post} key={post.id}/>
-                </Link>
+                    <PostCard post={post} key={post.id} pathname={location.pathname}/>
             )
         })}
       </div>
     ) :
-    <div>
+    <div className='administrators-only-section'>
       <h1>This page is for administrators only.</h1>
     </div>
   );
