56882a16bfa75cf29335862b8cda4f85b90e4fec
Jacob Layton <jlayjlay3@gmail.com>
2021-05-28 14:48:23 -0700

    Update Blog post page

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import '../styles/blog-post.css';
 import { useAuth0 } from "@auth0/auth0-react";
+import moment from 'moment';
 
 function BlogPost({ match }) {
     const { isAuthenticated, isLoading } = useAuth0();
@@ -10,6 +12,16 @@ function BlogPost({ match }) {
     } = match;
     // const [isLoading, setIsLoading] = useState(true);
     const [postData, setPostData] = useState([]);
+    let displayCategory;
+    if (postData.category === 'product_reviews') {
+        displayCategory = 'Product Review';
+    } else if (postData.category === 'lifestyle') {
+        displayCategory = 'Lifestyle';
+    } else if (postData.category === 'for_the_home') {
+        displayCategory = 'For The Home';
+    } else {
+        displayCategory = 'General';
+    }
 
     useEffect(() => {
         let mounting = true;
@@ -17,7 +29,11 @@ function BlogPost({ match }) {
 			.then(res =>  {
 				console.log('API CALLED', res);
                 if(mounting) {
-                    setPostData(res.data[0]);
+                    const rawPostData = res.data[0];
+                    const createdDate = moment(rawPostData.created_date).format('MMMM Do, YYYY');
+                    rawPostData.created_date = createdDate;
+                    setPostData(rawPostData);
+                    
                 }
 			})
 			.catch(err => {
@@ -32,25 +48,13 @@ function BlogPost({ match }) {
 
 
   return (
-    isAuthenticated ? (
-        <div className='blog-post-container'>
-            <button>Edit</button>
-            <button>Delete</button>
-            <h1>{postData.title}</h1>
-            <h5>{postData.category}</h5>
-            <h5>{postData.created_date}</h5>
-            <img src={postData.image_url} alt=""/>
-            <p>{postData.content}</p>
-        </div>
-    ) : (
-        <div className='blog-post-container'>
-            <h1>{postData.title}</h1>
-            <h5>{postData.category}</h5>
-            <h5>{postData.created_date}</h5>
-            <img src={postData.image_url} alt=""/>
-            <p>{postData.content}</p>
-        </div>
-    )
+    <div className='blog-post-container'>
+        <h1>{postData.title}</h1>
+        <img src={postData.image_url} alt=""/>
+        <h5><Link to={`/category/${postData.category}`}>{displayCategory}</Link></h5>
+        <h5>{postData.created_date}</h5>
+        <div dangerouslySetInnerHTML={{ __html: postData.content }} className="post-data"/>
+    </div>
   )
 }
 
