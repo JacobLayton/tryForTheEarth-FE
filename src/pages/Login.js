cc997d32f9fe1e84de12c5a3ca813a17259a5499
Jacob Layton <jlayjlay3@gmail.com>
2021-06-09 14:53:01 -0700

    Update Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
-import LoginButton from '../components/Login-Button'
+import LoginButton from '../components/Login-Button';
+import '../styles/login-page.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
+  const { logout } = useAuth0();
   return (
-    <LoginButton />
+    <div className='login-container'>
+      <div className='login-content'>
+        <h1>Welcome back Taylin!</h1>
+        <LoginButton />
+        <span
+          className='mini-log-out'
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Log Out
+        </span>
+      </div>
+    </div>
   );
 }
 
