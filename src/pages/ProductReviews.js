50819b24598f8b04d49c284b8754917330c01e9f
Jacob Layton <jlayjlay3@gmail.com>
2021-06-26 14:57:50 -0700

    Create Production Reviews posts page

---
+++ src/pages/ProductReviews.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import PostCard from "../components/PostCard";
+import '../styles/category-pages.css';
+
+function ProductReviews(props) {
+    const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+        let mounting = true;
+		axios.get('http://localhost:9001/posts/category/product_reviews')
+			.then(res =>  {
+				console.log('API CALLED', res);
+                if(mounting) {
+                    setPosts(res.data);
+                }
+			})
+			.catch(err => {
+				console.log('Error in get request', err);
+			})
+        return () => mounting = false;
+	  }, []);
+    console.log('POSTS: ', posts);
+  return (
+    <div className='category-page'>
+        <div className='category-header'>
+            <h1 className='category-title'>Product Reviews</h1>
+            <div className='category-line-break' />
+        </div>
+        {posts.map(post => {
+            return ( 
+                // <Link to={`/blogpost/${post.id}`} key={post.id}>
+                //     <PostCard post={post} key={post.id}/>
+                // </Link>
+                <PostCard post={post} key={post.id}/>
+            )
+        })}
+    </div>
+    );
+}
+
+export default ProductReviews;
