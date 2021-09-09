872386dc44703c16301d96f62f4832930a8954f7
Jacob Layton <jlayjlay3@gmail.com>
2021-09-08 21:34:55 -0700

    Update contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,14 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
+import '../styles/contact-page.css';
+
 
 function Contact(props) {
-  return <div>Contact Page</div>;
+  return (
+    <div className="contact-section-mobile">
+      <ContactForm />
+    </div>
+  );
 }
 
 export default Contact;
