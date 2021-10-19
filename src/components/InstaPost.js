3663d84cc7c8afc1f43bd9f49eaa2ef8d7a4dbfa
Jacob Layton <jlayjlay3@gmail.com>
2021-10-18 21:40:54 -0700

    Add instagram post component

---
+++ src/components/InstaPost.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const InstaPost = (props) => {
+    const { id, caption, media_type, media_url, timestamp, thumbnail_url, permalink} = props.feed
+    let post;
+
+    switch (media_type) {
+        case "VIDEO":
+            post = (
+                <video
+                    src={media_url} 
+                    type="video/mp4" 
+                    controls playsinline>
+                </video>
+            )
+            break;
+        case "CAROUSEL_ALBUM":
+            post = (
+                <img 
+                    id={id} 
+                    src={media_url} 
+                    alt={caption}
+                    className='insta-photo'
+                />
+            );
+            break;
+        default:
+            post = (
+                <img 
+                    id={id} 
+                    src={media_url} 
+                    alt={caption}
+                    className='insta-photo'
+                />
+            );
+    }       
+
+    return (
+        <React.Fragment>
+            <Link to={{ pathname: permalink }} target="_blank">
+                {post}
+            </Link>
+        </React.Fragment>
+    );
+}
+
+export default InstaPost;
