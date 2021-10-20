9b6e72787e90424c34f0defb3b8f3fd9b254903f
Jacob Layton <jlayjlay3@gmail.com>
2021-10-19 21:41:23 -0700

    Add instagram section component

---
+++ src/components/InstaSection.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
+import axios from "axios";
+import InstaPost from "./InstaPost";
+
+function InstaSection(props) {
+    const [instaPosts, setInstaPosts] = useState([]);
+    useEffect(() => {
+        async function fetchInstaPosts() {
+          try {
+            axios.get(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,timestamp,caption,thumbnail_url,permalink&limit=${process.env.REACT_APP_INSTA_LIMIT}&access_token=${process.env.REACT_APP_INSTA_TOKEN}`)
+            .then((res) => {
+              setInstaPosts(res.data.data);
+            })
+          } catch (err) {
+            console.log('ERROR: ', err);
+          }
+        }
+        fetchInstaPosts();
+      }, []);
+
+  return (
+    <div className='picture-section'>
+        {
+            instaPosts.length > 1 ?
+                instaPosts.map((instaPost) => {
+                    return <InstaPost key={instaPost.id} feed={instaPost} />
+                })
+            : <h1> Loading...</h1>
+        }
+    </div>
+  );
+}
+
+export default InstaSection;
