8f50cbb2d65f45824acab0ef19ac5be45be6843d
Jacob Layton <jlayjlay3@gmail.com>
2022-01-12 20:24:44 -0800

    Update auth0 login to popup

--- src/components/Login-Button.js
+++ src/components/Login-Button.js
@@ -3,9 +3,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import '../styles/components.css';
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithPopup } = useAuth0();
 
-  return <button id='login-button' onClick={() => loginWithRedirect({
+  return <button id='login-button' onClick={() => loginWithPopup({
     redirectUri: 'https://www.tryfortheearth.com/admin'
   })}>Log In</button>;
 };
