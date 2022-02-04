daffb93e07af21f9ce0ea23c1671a8a1dc478968
Jacob Layton <jlayjlay3@gmail.com>
2022-02-03 19:38:39 -0800

    Change product reviews to product mentions

--- src/pages/ProductReviews.js
+++ src/pages/ProductReviews.js
@@ -15,6 +15,7 @@ function ProductReviews(props) {
 			.then(res =>  {
                 if(mounting) {
                     setPosts(res.data);
+                    handleScrollPosition();
                 }
 			})
 			.catch(err => {
@@ -42,10 +43,17 @@ function ProductReviews(props) {
             )
         })
     })
+    function handleScrollPosition() {
+        const scrollPosition = sessionStorage.getItem('scrollPosition');
+        if (scrollPosition) {
+          window.scrollTo(0, parseInt(scrollPosition));
+          sessionStorage.removeItem("scrollPosition");
+        }
+    }
   return (
     <div className='category-page'>
         <div className='category-header'>
-            <h1 className='category-title'>Product Reviews</h1>
+            <h1 className='category-title'>Product Mentions</h1>
             <div className='category-line-break' />
         </div>
         <div className='category-cards'>
