d6e43505ba28042bdb771606a5ff6fef32e348db
Jacob Layton <jlayjlay3@gmail.com>
2022-02-07 19:42:08 -0800

    Remove extra content from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/footer.css';
 
 
@@ -8,22 +8,22 @@ const Footer = (props) => {
   return (
     <div className='footer-container'>
         <div className='footer-content'>
-            <h1 className='footer-text'>Join The Community</h1>
-            <h4 className='footer-text'>Subscribe for my sustainability newsletter</h4>
+            {/* <h1 className='footer-text'>Join The Community</h1>
+            <h4 className='footer-text'>Subscribe for my sustainability newsletter</h4> */}
             <div className='footer-interactive'>
-                <div className='footer-form'>
+                {/* <div className='footer-form'>
                     <input type='email' placeholder='EMAIL ADDRESS'/>
                     <button>Join!</button>
-                </div>
-                <div className='footer-social-icons'>
+                </div> */}
+                {/* <div className='footer-social-icons'>
                     <FontAwesomeIcon icon={['fab', 'facebook-f']} size="lg" className='social-icon'/>
                     <FontAwesomeIcon icon={['fab', 'instagram']} size="lg" className='social-icon'/>
                     <FontAwesomeIcon icon={['fab', 'pinterest-p']} size="lg" className='social-icon'/>
                     <FontAwesomeIcon icon={['fab', 'twitter']} size="lg" className='social-icon'/>
                     <FontAwesomeIcon icon={['fab', 'youtube']} size="lg" className='social-icon'/>
-                </div>
+                </div> */}
                 <div className='footer-copyright'>
-                    <span className='footer-text'>© 2021 TRY FOR THE EARTH</span>
+                    <span className='footer-text'>© 2021 TRYFORTHEEARTH</span>
                     <span className='footer-text'>ALL RIGHTS RESERVED</span>
                     <span className='footer-text'>DESIGN AND DEVELOPMENT BY <a className='footer-text footer-link' href="http://www.jacoblayton.dev" target="_blank" rel="noreferrer">JACOB LAYTON</a></span>
                 </div>
