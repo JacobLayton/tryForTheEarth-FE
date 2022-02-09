127e60197ec8895f90d1263ce309794680821bf5
Jacob Layton <jlayjlay3@gmail.com>
2022-02-08 19:42:39 -0800

    Scroll issue on nav buttons

--- src/components/NavButtonsDesktop.js
+++ src/components/NavButtonsDesktop.js
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import '../styles/nav-buttons-desktop.css';
 
 const NavButtonsDesktop = (props) => {
+  function resetScrollPosition() {
+    sessionStorage.removeItem("scrollPosition");
+  }
 
   return (
     <div className='nav-buttons-desktop'>
       <div className='desktop-home-button'>
-          <Link to='/'>Home</Link>
+          <Link to='/' onClick={resetScrollPosition}>Home</Link>
       </div>
       <div className='desktop-category-buttons'>
-        <Link to='/category/product_reviews'>Product Reviews</Link>
-        <Link to='/category/for_the_home'>For The Home</Link>
-        <Link to='/category/lifestyle'>Lifestyle</Link>
+        <Link to='/lifestyle' onClick={resetScrollPosition}>Lifestyle</Link>
+        <Link to='/product_mentions' onClick={resetScrollPosition}>Product Mentions</Link>
+        <Link to='/homemade' onClick={resetScrollPosition}>Homemade</Link>
       </div>
     </div>
   );
