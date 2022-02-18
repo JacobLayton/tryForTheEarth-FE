d3c3e794258139cbda9181c007d779ea19d37e32
Jacob Layton <jlayjlay3@gmail.com>
2022-02-17 19:48:40 -0800

    Update for fontawesome

---
+++ src/fontawesome.js
@@ -0,0 +1,17 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+// import { faMoneyBill, faTwitter } from '@fortawesome/pro-solid-svg-icons';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faYoutube, faInstagram, faFacebookF, faPinterestP } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+    faTwitter,
+    faYoutube,
+    faInstagram,
+    faFacebookF,
+    faPinterestP,
+    faAngleUp,
+  // more icons go here
+);
